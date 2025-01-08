9089325d4691b39037b40d0177db019b748a8ddb
Tyler Connelly <tconnelly@gsudoers.com>
2025-01-08 16:46:46 -0500

    fix ec2 tests

--- aws_rego/ec2_checks/ec2_old_snapshots/ec2_old_snapshots/test_old_snapshots.py
+++ aws_rego/ec2_checks/ec2_old_snapshots/ec2_old_snapshots/test_old_snapshots.py
@@ -9,10 +9,10 @@ def test_idle_instances(rego_process):
     current_dir = pathlib.Path(os.path.abspath(__file__)).parent
 
     rego_policy = os.path.join(current_dir, "ec2_old_snapshots.rego")
-    rego_input = os.path.join(current_dir.parent, "ec2_test_data.json")
+    rego_input = os.path.join(current_dir.parent.parent, "ec2_test_data.json")
 
     needed_keys = ["progress", "region", "snapshot_id", "start_time", "state", "tags", "volume_id"]
-    result = rego_process(rego_policy, rego_input, "data.aws.cost.ec2_old_snapshots", ["ec2_old_snapshots"])
+    result = rego_process(rego_policy, rego_input, "aws_rego.ec2_checks.ec2_old_snapshots.ec2_old_snapshots", ["ec2_old_snapshots"])
 
     for x in result["ec2_old_snapshots"]:
         for key in needed_keys:
