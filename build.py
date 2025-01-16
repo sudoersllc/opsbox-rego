d967aa09e6ef731d98cb3c9bb6dd0c632b443499
Tyler Connelly <tconnelly@gsudoers.com>
2025-01-15 19:13:16 -0500

    Add improved build script

--- build.py
+++ build.py
@@ -2,37 +2,59 @@ import os
 import shutil
 import subprocess
 from tqdm import tqdm
+import argparse
+from loguru import logger
+import sys
 
-def find_projects_with_pyproject(base_dir):
-    """Find all subdirectories containing a pyproject.toml file before hitting the first one and stop traversing subdirectories of those directories."""
+
+def find_projects_with_pyproject(base_dir: str) -> list:
+    """Find all subdirectories containing a pyproject.toml file before hitting the first one and stop traversing subdirectories of those directories.
+
+    Args:
+        base_dir (str): The directory to start searching for projects with pyproject.toml files.
+
+    Returns:
+        A list of directories containing pyproject.toml files.
+    """
     projects = []
+
     def traverse_directory(directory):
-        flip = False
         for root, dirs, files in os.walk(directory):
-            if 'pyproject.toml' in files and flip is True:
+            if "pyproject.toml" in files:
                 projects.append(root)
                 # Stop traversing subdirectories of this directory
                 dirs.clear()
                 continue
-            elif 'pyproject.toml' in files:
-                flip = True
-                continue
 
     traverse_directory(base_dir)
-    print(f"Found {len(projects)} projects with pyproject.toml")
     return projects
 
-def build_project(project_dir):
-    """Run `python -m build` in the specified project directory."""
+
+def build_project(project_dir: str):
+    """Run `uv build` in the specified project directory.
+
+    Args:
+        project_dir (str): The directory to run `uv build` in.
+    """
     try:
         subprocess.run(
-            ["uv", "build"], cwd=project_dir, check=True, stdout=subprocess.DEVNULL, stderr=subprocess.PIPE
+            ["uv", "build"],
+            cwd=project_dir,
+            check=True,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.PIPE,
         )
     except subprocess.CalledProcessError as e:
         print(f"Failed to build project in {project_dir}: {e.stderr.decode().strip()}")
 
-def collect_dist_files(projects, common_dist_dir):
-    """Copy dist files from each project into a common dist directory."""
+
+def collect_dist_files(projects: list[str], common_dist_dir: str):
+    """Copy dist files from each project into a common dist directory.
+
+    Args:
+        projects (list[str]): A list of directories containing dist files.
+        common_dist_dir (str): The directory to copy dist files to.
+    """
     if not os.path.exists(common_dist_dir):
         os.makedirs(common_dist_dir)
 
@@ -47,25 +69,112 @@ def collect_dist_files(projects, common_dist_dir):
                 if os.path.abspath(source_file) != os.path.abspath(destination_file):
                     shutil.copy2(source_file, destination_file)  # Overwrites if exists
 
+
+def clean_dist_files(projects: list[str], common_dist_dir: str):
+    """Clean dist. files and folders from subdir dist directories.
+
+    Args:
+        projects (list[str]): A list of directories containing dist files.
+        common_dist_dir (str): The directory to clean dist files from.
+    """
+    for project in projects:
+        dist_dir = os.path.join(project, "dist")
+        if os.path.exists(dist_dir):
+            shutil.rmtree(dist_dir, ignore_errors=True)
+
+    if os.path.exists(common_dist_dir):
+        shutil.rmtree(common_dist_dir, ignore_errors=True)
+
+
+def add_args():
+    """Add arguments to the argument parser."""
+    parser = argparse.ArgumentParser(
+        description="Build projects and collect dist files."
+    )
+    parser.add_argument(
+        "--build-dir",
+        type=str,
+        default=os.getcwd(),
+        help="The directory to look for projects to build.",
+    )
+    parser.add_argument(
+        "--clean",
+        action="store_true",
+        help="Clean the dist directory before collecting dist files.",
+    )
+    parser.add_argument(
+        "--dist-dir",
+        type=str,
+        default=os.path.join(os.getcwd(), "dist"),
+        help="The directory to collect dist files in.",
+    )
+    parser.add_argument(
+        "--verbose",
+        action="store_true",
+        help="Enable verbose logging.",
+    )
+    return parser
+
+
 def main():
-    base_dir = os.getcwd()
-    common_dist_dir = os.path.join(base_dir, "dist")
+    # setup logger
+    logger.remove()
+    args = add_args().parse_args()  # Parse arguments
 
-    print("Finding projects with pyproject.toml...")
-    projects = find_projects_with_pyproject(base_dir)
+    if args.verbose:
+        logger.add(
+            sink=sys.stdout,
+            format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {message}",
+            level="DEBUG",
+        )
+    else:
+        logger.add(
+            sink=sys.stdout,
+            format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {message}",
+            level="INFO",
+        )
 
-    if not projects:
-        print("No projects with pyproject.toml found.")
+    # Set the base directory and common dist directory
+    base_dir = args.build_dir
+    common_dist_dir = args.dist_dir
+
+    logger.debug(f"Base directory: {base_dir}")
+    logger.debug(f"Common dist directory: {common_dist_dir}")
+
+    # Gather subdirectories of the base directory
+    subdirs = [
+        os.path.join(base_dir, name)
+        for name in os.listdir(base_dir)
+        if os.path.isdir(os.path.join(base_dir, name))
+    ]
+
+    # Find projects with pyproject.toml in subdirectories
+    projects = []
+    for subdir in subdirs:
+        logger.debug(f"Finding projects with pyproject.toml in {subdir}")
+        items = find_projects_with_pyproject(subdir)
+        logger.info(f"Found {len(items)} projects with pyproject.toml in {subdir}")
+        projects.extend(items)
+
+    if len(projects) == 0:
+        logger.critical(f"No projects with pyproject.toml found in {base_dir}")
         return
 
-    print("Building project...")
-    for project in tqdm(projects, desc="Building project", unit="project"):
+    # Clean dist files before collecting
+    if args.clean:
+        logger.info("Cleaning dist files...")
+        clean_dist_files(projects, common_dist_dir)
+
+    # Build projects and collect dist files
+    logger.info("Starting build process...")
+    for project in tqdm(projects, desc="Building Project", unit="project"):
         build_project(project)
 
-    print("Collecting dist files...")
+    logger.info("Collecting dist files...")
     collect_dist_files(projects, common_dist_dir)
 
-    print(f"All dist files collected in {common_dist_dir}")
+    logger.success(f"All dist files collected in {common_dist_dir}")
+
 
 if __name__ == "__main__":
     main()
