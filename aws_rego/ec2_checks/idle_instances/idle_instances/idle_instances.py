6bbf0b50b79a010374140e8a511c76bd77b45229
Tyler Connelly <tconnelly@gsudoers.com>
2025-01-23 21:47:54 -0500

    parameterize EC2 checks

--- aws_rego/ec2_checks/idle_instances/idle_instances/idle_instances.py
+++ aws_rego/ec2_checks/idle_instances/idle_instances/idle_instances.py
@@ -1,11 +1,55 @@
 import yaml
 from loguru import logger
 from core.plugins import Result
+from pydantic import BaseModel, Field
+from typing import Annotated
+from datetime import datetime, timedelta
+from pluggy import HookimplMarker
 
+# Define a hookimpl (implementation of the contract)
+hookimpl = HookimplMarker("opsbox")
+
+
+class EC2IdleInstancesConfig(BaseModel):
+    ec2_cpu_idle_threshold: Annotated[
+        int,
+        Field(
+            default=1,
+            description="The % threshold for the average CPU utilization of an EC2 instance to be considered idle. Default is 1.",
+        ),
+    ]
 
 class IdleInstances:
     """Plugin for identifying idle EC2 instances."""
 
+    @hookimpl
+    def grab_config(self) -> type[BaseModel]:
+        """Return the plugin's configuration pydantic model.
+        These should be things your plugin needs/wants to function."""
+        return EC2IdleInstancesConfig
+
+    @hookimpl
+    def set_data(self, model: type[BaseModel]) -> None:
+        """Set the data for the plugin based on the model.
+
+        Args:
+            model (BaseModel): The model containing the data for the plugin."""
+        self.conf = model.model_dump()
+
+    @hookimpl
+    def inject_data(self, data: "Result") -> "Result":
+        """Inject data into the plugin.
+
+        Args:
+            data (Result): The data to inject into the plugin.
+
+        Returns:
+            Result: The data with the injected values.
+        """
+        data.details["input"]["ec2_cpu_idle_threshold"] = self.conf["ec2_cpu_idle_threshold"]
+        return data
+
+    @hookimpl
     def report_findings(self, data: Result) -> Result:
         """Report the findings of the plugin.
         Attributes:
--- aws_rego/ec2_checks/idle_instances/idle_instances/test_idle_instances.py
+++ aws_rego/ec2_checks/idle_instances/idle_instances/test_idle_instances.py
@@ -1,3 +1,4 @@
+import json
 import os
 import pathlib
 
@@ -5,7 +6,23 @@ def test_idle_instances(rego_process):
     """Test for idle instances rego policy"""
     # Load rego policy
     current_dir = pathlib.Path(os.path.abspath(__file__)).parent
+    
+    # if test key does not exist in the result, the test will fail.
+    # we need to add ec2_cpu_idle_threshold to the json file.
+    write: bool = False
+    test_data = os.path.join(current_dir.parent.parent, "ec2_test_data.json")
+    with open(test_data, "r") as file:
+        data = json.load(file)
+        if "ec2_cpu_idle_threshold" not in data:
+            data["ec2_cpu_idle_threshold"] = 1
+            write = True
 
+    # overwrite the file
+    if write:
+        with open(test_data, "w") as file:
+            json.dump(data, file, indent=4)
+
+    
     rego_policy = os.path.join(current_dir, "idle_instances.rego")
     rego_input = os.path.join(current_dir.parent.parent, "ec2_test_data.json")
     needed_keys = ["avg_cpu_utilization", "ebs_optimized", "instance_id", "instance_type", "operating_system", "processor", "region", "state", "tags", "tenancy", "virtualization_type"]
