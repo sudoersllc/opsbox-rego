d99ac515113f738b8a3edea22cd10b98a3d931a4
Tyler Connelly <tconnelly@gsudoers.com>
2025-01-28 18:35:37 -0500

    denest, decruft, and clean out old r53 directory structure

--- aws_rego/r53_checks/empty_zones/test_empty_zones.py
+++ aws_rego/r53_checks/empty_zones/test_empty_zones.py
@@ -9,7 +9,7 @@ def test_unused_policies(rego_process):
     current_dir = pathlib.Path(os.path.abspath(__file__)).parent
 
     rego_policy = os.path.join(current_dir, "empty_zones.rego")
-    rego_input = os.path.join(current_dir.parent.parent, "r53_test_data.json")
+    rego_input = os.path.join(current_dir.parent, "r53_test_data.json")
 
     needed_keys = ["id", "name", "private_zone", "record_count"]
     result = rego_process(rego_policy, rego_input, "data.aws.cost.unused_policies", ["empty_hosted_zones"])
